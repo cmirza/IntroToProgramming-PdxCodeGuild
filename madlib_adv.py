455e05206aac30e3fbfbdf0876043ce60b1863a3
Cameron Mirza <mail@cmirza.com>
2017-08-18 19:05:01 -0700

    Add files via upload

---
+++ madlib_adv.py
@@ -0,0 +1,7 @@
+input_adj = input("Give me three adjectives seperated by commas:\n")
+input_adj.split(", ")
+adj_1,adj_2,adj_3 = input_adj.split(", ")
+
+print("Adjective 1: " + adj_1 + "\n")
+print("Adjective 2: " + adj_2 + "\n")
+print("Adjective 3: " + adj_3 + "\n")
