607e66e53639ed45002a61be799484a8c1544435
Cameron Mirza <mail@cmirza.com>
2017-08-18 19:12:59 -0700

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,17 @@
+input_grade = input("What percentage did you get:")
+grade = int(input_grade)
+
+if grade > 100:
+    print("I don't think so...\n")
+elif grade >= 90:
+    print("You got an A!\n")
+elif grade >= 80:
+    print("You got an B!\n")
+elif grade >= 70:
+    print("You got an C!\n")
+elif grade >= 60:
+    print("You got an D!\n")
+elif grade >= 0:
+    print("You got an F!\n")
+elif grade < 0:
+    print("I don't think so...\n")
