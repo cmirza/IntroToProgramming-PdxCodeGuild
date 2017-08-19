001cec8c52c37f0d3c744db49bceea11c0ba7d34
Cameron Mirza <mail@cmirza.com>
2017-08-18 19:08:30 -0700

    Add files via upload

---
+++ rand_emot.py
@@ -0,0 +1,7 @@
+import random
+
+eyes_list = [';', ':', '#']
+nose_list = ['-', '^', '@']
+mouth_list = [')', '(', '|']
+
+print(random.choice(eyes_list)+random.choice(nose_list)+random.choice(mouth_list))
