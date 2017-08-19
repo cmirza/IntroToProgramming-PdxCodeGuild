fa3fc39ef5630e30e8410d6b1d974434483b9935
Cameron Mirza <mail@cmirza.com>
2017-08-18 19:15:42 -0700

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+pass_length = input('How long of a password do you need?\n')
+pass_length = int(pass_length)
+
+pass_chars = string.ascii_letters + string.digits
+
+for i in range(pass_length):
+    print(random.choice(pass_chars), end="")
+print("\n")
