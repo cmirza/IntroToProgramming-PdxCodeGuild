610093f48a2d83214def42095456a34ec4f3cd5e
Cameron Mirza <mail@cmirza.com>
2018-02-13 13:30:45 -0800

    fix contributions

--- Learn_More_Python_The_Hard_Way/ex47_bc/tests/test_scanner.py
+++
@@ -1,8 +0,0 @@
-from calc.run import TOKENS
-from calc.scanner import Scanner
-
-code = open("test2.calc").readlines()
-
-def test_Scanner():
-    s = Scanner(TOKENS, code)
-
--- Learn_More_Python_The_Hard_Way/ex49_sed/tests/test_basics.py
+++
@@ -1,6 +0,0 @@
-from ed import ed
-import os
-
-def test_process():
-    pass
-
